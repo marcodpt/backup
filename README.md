9c52eeea795c3b054596373811160792a8efdc86
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2021-07-27 18:26:15 -0300

    First release

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # backup
-File backup script in python3 with JSON config file
+> File backup script in python3 with JSON config file
+
+## Usage
+```sh
+./dump.py
+```
+
+## config.json
+ - array of objects: items properties:
+   - string `source`: the source file, maybe be remote machine
+`user`@`IP`:`filePath`
+   - string `target`: the target file, always local machine, use `*` for
+current timestamp
+   - number `limit`: the max number of files of this type on the target dir
+   - boolean `compress`: should be compressed
+
+## decompress backup
+```sh
+tar -zxf {filename}
+```
---
+++ dump.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import json
+import os 
+from datetime import datetime
+
+def run(cmd):
+    os.system(cmd)
+
+path = 'config.json'
+
+if not os.path.isfile(path):
+    print("Error: Unable to locate file <"+path+">")
+    quit()
+    
+with open(path) as filePtr:    
+    Actions = json.load(filePtr)
+
+def putTime(name):
+    iso = str(datetime.now()).replace(" ", "T", 1)
+    iso = iso[0:16].replace(':', '_')
+    return name.replace('*', iso)
+
+for Action in Actions:
+    fullPath = putTime(Action['target'])
+    T = fullPath.split('/')
+    target = T.pop()
+    os.chdir('/'.join(T))
+    print ("***** Copying: "+target+" *****")
+    run('scp '+Action['source']+' '+target)
+    ext = ''
+    if 'compress' in Action and Action['compress']:
+        ext = '.tar.gz'
+        print ("***** Compressing: "+target+ext+" *****")
+        run('tar -zcf '+target+ext+' '+target)
+        run('rm '+target)
+    if 'limit' in Action and Action['limit'] > 0:
+        files = os.popen(
+            "ls "+Action['target']+ext+' 2>/dev/null'
+        ).read().split("\n")
+        files.pop()
+        i = 0
+        while i < len(files) - Action['limit']:
+            print ("***** Remove: "+files[i]+" *****")
+            run('rm '+files[i])
+            i = i + 1
